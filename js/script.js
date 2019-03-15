038df6f64ff9c1f9390f555615623d4317a8d35a
FRAIS <nikixodr144@gmail.com>
2019-03-15 21:29:39 +0500

    3 commit

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
     //Timer
 
-    let deadline = '2019-03-15';
+    let deadline = '2019-03-16';
 
     function TimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -88,4 +88,32 @@ window.addEventListener('DOMContentLoaded', function(){
     }
 }
     setClock('timer', deadline);
+
+
+    //modal
+
+
+    let more = document.querySelector('.more');
+    let overlay = document.querySelector(".overlay");
+    let close = document.querySelector(".popup-close");
+
+    more.addEventListener('click', function() {
+        overlay.style.display = "block";
+        this.classList.add('more-splash');
+        document.body.style.overflow = "hidden";
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = "none";
+        more.classList.remove('more-splash');
+        document.body.style.overflow = "";
+    });
+
+    //let age = document.getElementById('age');
+
+    //function showUser(surname, name) {
+    //	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+    //}
+
+   //showUser.apply(age,["Моргенштерн", "Алишер"]);
 });
